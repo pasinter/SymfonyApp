46a2ac974a971ead475f8141a196d86b915ce76a
Ken Golovin <kenneth.golovin@lp-digital.fr>
2014-11-04 17:54:00 +0100

    updates

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,11 +16,44 @@ class AppKernel extends Kernel
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+			
+			
+			// sonata
+            new Sonata\CoreBundle\SonataCoreBundle(),
+            new Sonata\EasyExtendsBundle\SonataEasyExtendsBundle(),
+            new Sonata\AdminBundle\SonataAdminBundle(),
+            //new Sonata\DoctrineMongoDBAdminBundle\SonataDoctrineMongoDBAdminBundle(),
+            new Sonata\BlockBundle\SonataBlockBundle(),
+            new Sonata\CacheBundle\SonataCacheBundle(),
+            new Sonata\NotificationBundle\SonataNotificationBundle(),
+			new Sonata\IntlBundle\SonataIntlBundle(),
+            
+            // images
+            new Liip\ImagineBundle\LiipImagineBundle(),
+            
+			
+			// twitter bootstrap etc
+            new Mopa\Bundle\BootstrapBundle\MopaBootstrapBundle(),
+            new Knp\Bundle\PaginatorBundle\KnpPaginatorBundle(),
+			new Knp\Bundle\MenuBundle\KnpMenuBundle(),
+			
+			// Doctrine behaviours
+            new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
+			
+			// locale
+            new Lunetics\LocaleBundle\LuneticsLocaleBundle(),
+			
+			// Security
+            new JMS\AopBundle\JMSAopBundle(),
+            new JMS\SecurityExtraBundle\JMSSecurityExtraBundle(),
+            new JMS\DiExtraBundle\JMSDiExtraBundle($this),
         );
+		
+
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
-            $bundles[] = new Acme\DemoBundle\AcmeDemoBundle();
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
+			$bundles[] = new Elao\WebProfilerExtraBundle\WebProfilerExtraBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
         }
