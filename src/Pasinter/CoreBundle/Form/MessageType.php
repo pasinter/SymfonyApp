00e51842b19131a4578d2d45e71a0f5d98c5eae3
pasinter <ken.golovin@gmail.com>
2014-12-15 20:20:48 +0100

    message form type

---
+++ src/Pasinter/CoreBundle/Form/MessageType.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Pasinter\CoreBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class MessageType extends AbstractType
+{
+        /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('body')
+            ->add('thread')
+            ->add('sender')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Pasinter\CoreBundle\Entity\Message'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'pasinter_corebundle_message';
+    }
+}
